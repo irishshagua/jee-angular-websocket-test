6dddf23cbd0971b4b0fdf27bed6e05f017886437
Brian Mooney <brian.mooney@paddypower.com>
2015-01-29 09:57:03 +0000

    Update JS to bind dom element to ws response

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -1,5 +1,10 @@
-angular.module('StatusReaderApp.controllers', []).
-	controller('StatusController', [function($scope, WebSocketStatusService) {
-		$scope.status = WebSocketStatusService.statusUpdate;
-	}]
+var appControllers = angular.module('StatusReaderApp.controllers', ['StatusReaderApp.services']);
+
+appControllers.controller('StatusController', function ($scope, $rootScope, WebSocketStatusService) {
+        WebSocketStatusService.openPipe();
+
+        $rootScope.$watch('statusUpdate', function () {
+            $scope.status = $rootScope.statusUpdate;
+        });
+    }
 );
